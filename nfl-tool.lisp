7bba8b97a68471dbf37711e8de0435ad9d360755
Paul B. Krause <paul.b.krause@gmail.com>
2024-07-14 09:01:49 -0400

    Cleaning up spacing around team icon panel

--- nfl-tool.lisp
+++ nfl-tool.lisp
@@ -34,13 +34,15 @@
 
 (defconstant +game-list-item-top-border-size+ 4)
 (defconstant +game-list-item-bottom-border-size+ 4)
-(defconstant +game-list-item-inner-border-size+ 4)
+(defconstant +game-list-item-west-border-size+ 2)
+(defconstant +game-list-item-inner-border-size+ 2)
 (defconstant +game-list-item-date-padding+ 4)
 
-(defconstant +game-day-info-x-offset+ (* 2 (+ +icon-small+
-                                              +game-list-item-inner-border-size+
-                                              +game-list-item-date-padding+)))
-(defconstant +game-airer-info-x-offset+ 200)
+(defconstant +game-day-info-x-offset+ (+ (* +icon-small+ 2)
+                                         +game-list-item-west-border-size+
+                                         +game-list-item-inner-border-size+
+                                         +game-list-item-date-padding+))
+(defconstant +game-airer-info-x-offset+ 150)
 
 (defconstant nfc_color (make-rgb-color 0.0 (/ 59 256) (/ 37 102)))
 (defconstant afc_color (make-rgb-color (/ 206 256) (/ 19 256) (/ 102 256)))
@@ -60,11 +62,6 @@
   (let ( (color (car (cdr (team-colors team)))) )
      (make-rgb-color (/ (aref color 0) 255) (/ (aref color 1) 255) (/ (aref color 2) 255))))
 
-(defun make-team-icon-pane-xsmall (team) (make-team-icon-pane team +icon-xsmall+))
-(defun make-team-icon-pane-small  (team) (make-team-icon-pane team +icon-small+))
-(defun make-team-icon-pane-large  (team) (make-team-icon-pane team +icon-large+))
-(defun make-team-icon-pane-xlarge (team) (make-team-icon-pane team +icon-xlarge+))
-
 (defun resolve-airer-png-name (airer)
   (cond
     ( (eq airer :espn+) "ESPN_plus" )
@@ -83,39 +80,43 @@
 ;; %% TEAM ICON PANE ---------------------------------------------------------------------------------------------------
 
 (defclass team-icon-pane (clim-stream-pane)
-  ( (team :initarg :team)
-    (size :initarg :size)
-    (logo :initarg :logo)
-    (bg   :initarg :bg)
-    (hl   :initarg :hl) )
+  ( (team  :initarg :team)
+    (size  :initarg :size)
+    (north :initarg :north)
+    (south :initarg :south)
+    (east  :initarg :east)
+    (west  :initarg :west)
+    (bg    :initarg :bg)
+    (hl    :initarg :hl) )
 )
 
-(defun make-team-icon-pane (team size &optional invert)
+(defun make-team-icon-pane (team size &optional invert &key (north 0) (east 0) (south 0) (west 0))
   (let ( (main-color    (if invert (get-team-color-highlight team) (get-team-color-main team)))
          (second-color  (if invert (get-team-color-main team) (get-team-color-highlight team))) )
     (make-pane 'team-icon-pane :size size
                                :team team
+                               :north north
+                               :south south
+                               :east east
+                               :west west
                                :bg main-color
                                :hl second-color
                                :background main-color
-                               :max-width (+ (* 2 +game-list-item-inner-border-size+) size)
-                               :min-width (+ (* 2 +game-list-item-inner-border-size+) size)
-                               :max-height (+ +game-list-item-bottom-border-size+ +game-list-item-top-border-size+ size)
+                               :max-width (+ east west size)
+                               :min-width (+ east west size)
+                               :max-height (+ north south size)
                                :min-height size)))
 
 (defmethod handle-repaint ((pane team-icon-pane) region)
-  (with-slots (team size hl) pane
+  (with-slots (team size hl north south east west) pane
     (let ( (image (make-pattern-from-bitmap-file (team-logo-file team size))) )
       (clim:updating-output (pane)
-        (let ( (near-off (+ +border-adjust+ +border-off+))
-               (far-off-x (- (+ size (* 2 +game-list-item-inner-border-size+))
-                             (- (* 2 +border-off+) +border-adjust+)))
-               (far-off-y (- (+ size)
-                             (- (* 2 +border-off+) +border-adjust+))) )
-          (draw-rectangle* pane +border-off+ +border-off+ far-off-x far-off-y
+        (let ( (far-off-x (- (+ size east west) 7))
+               (far-off-y (- (+ size north south) 7)) )
+          (draw-rectangle* pane 6 6 far-off-x far-off-y
                                 :line-joint-shape :bevel
                                 :filled nil :line-thickness 4 :ink hl)
-          (draw-image* pane image +game-list-item-inner-border-size+ +game-list-item-top-border-size+))))))
+          (draw-image* pane image east north))))))
 
 ;; %% TEAM BANNER PANE -------------------------------------------------------------------------------------------------
 
@@ -190,13 +191,14 @@
       (if (cdr airer)
         (let ( (primary (car airer))
                (secondary (car (cdr airer))) )
-          (draw-text* pane (symbol-name primary) (- (- w +game-airer-info-x-offset+) 100) (floor h 2)
-                           :align-y :center
-                           :align-x :left)
-          (draw-text* pane (symbol-name secondary) (- w +game-airer-info-x-offset+) (floor h 2)
-                           :align-y :center
-                           :align-x :left)
-        )
+            (let ( (airer-icon-file-1 (airer-logo-file primary +icon-xsmall+))
+                   (airer-icon-file-2 (airer-logo-file secondary +icon-xsmall+)) )
+                (let ( (icon-1 (make-pattern-from-bitmap-file airer-icon-file-1))
+                       (icon-2 (make-pattern-from-bitmap-file airer-icon-file-2)) )
+                (draw-image* pane icon-1 (- w +game-airer-info-x-offset+) (+ +game-list-item-top-border-size+
+                                                                             (/ +icon-xsmall+ 2)))
+                (draw-image* pane icon-2 (- w (* 2 +game-airer-info-x-offset+)) (+ +game-list-item-top-border-size+
+                                                                                         (/ +icon-xsmall+ 2))))))
         (let ( (airer-icon-file (airer-logo-file (car airer) +icon-xsmall+)) )
           (let ( (airer-icon (make-pattern-from-bitmap-file airer-icon-file)) )
             (draw-image* pane airer-icon (- w +game-airer-info-x-offset+) (+ +game-list-item-top-border-size+
@@ -217,18 +219,22 @@
               (clim:updating-output (pane)
 ;               (draw-rectangle* pane 0 0 (+ +game-list-item-inner-border-size+ (* 2 +icon-small+)) h
 ;                                :ink (make-rgb-color 1 1 1)) 
-                (draw-rectangle* pane +game-list-item-inner-border-size+
+                (draw-rectangle* pane +game-list-item-west-border-size+
                                       +game-list-item-bottom-border-size+
-                                      (+ +game-list-item-inner-border-size+ +icon-small+)
+                                      (+ +game-list-item-west-border-size+ +icon-small+)
                                       (+ +game-list-item-top-border-size+ +icon-small+)
-                                      :line-joint-shape :bevel
+;                                     :line-joint-shape :bevel
                                       :filled t
                                       :ink away-bg)
-                (draw-rectangle* pane (+ +icon-small+ (* 2 +game-list-item-inner-border-size+))
+                (draw-rectangle* pane (+ +icon-small+
+                                         +game-list-item-west-border-size+
+                                         +game-list-item-inner-border-size+)
                                       +game-list-item-bottom-border-size+
-                                      (* 2 (+ +icon-small+ +game-list-item-inner-border-size+))
+                                      (+ (* 2 +icon-small+)
+                                              +game-list-item-west-border-size+
+                                              +game-list-item-inner-border-size+)
                                       (+ +game-list-item-top-border-size+ +icon-small+)
-                                      :line-joint-shape :bevel
+;                                     :line-joint-shape :bevel
                                       :filled t
                                       :ink home-bg)
                 (draw-line* pane 0 0 w 0 :ink +light-bg-color+)
@@ -237,35 +243,42 @@
                 (draw-game-day-info pane w h game)
                 (draw-game-time-info pane w h game)
                 (draw-game-airer-info pane w h game)
-                (draw-image* pane away-icon +game-list-item-inner-border-size+ +game-list-item-top-border-size+)
-                (draw-image* pane home-icon (+ +icon-small+ (* 2 +game-list-item-inner-border-size+))
+                (draw-image* pane away-icon +game-list-item-west-border-size+ +game-list-item-top-border-size+)
+                (draw-image* pane home-icon (+ +icon-small+
+                                               +game-list-item-west-border-size+
+                                               +game-list-item-inner-border-size+)
                                                +game-list-item-top-border-size+)))))
         (draw-text* pane "BYE" (floor w 2) (floor h 2) :text-size 20 :align-y :center :align-x :center)))))
 
 
 ;; %% SCHEDULE (LIST) PANE ---------------------------------------------------------------------------------------------
 
-; (defun make-team-schedule-pane (team)
-;   (make-pane :list-pane
-;              :mode :exclusive
-;              :items (team-schedule team)
-;              :value-changed-callback
-;                (lambda (pane item)
-;                  (if item
-;                    (format t "item changed: Game ~a @ ~a~%" (nfl-db::away-team item) (nfl-db::home-team item))
-;                    (format t "item changed: BYE ~%")))))
+(defun make-team-schedule-list-pane (team)
+  (make-pane :list-pane
+             :mode :exclusive
+             :items (team-schedule team)
+             :value-changed-callback
+               (lambda (pane item)
+                 (if item
+                   (format t "item changed: Game ~a @ ~a~%" (nfl-db::away-team item) (nfl-db::home-team item))
+                   (format t "item changed: BYE ~%")))))
 
 (defun make-team-schedule-pane (team)
   (let ( (games (team-schedule team)) )
     (make-pane :vbox-pane
                :contents (mapcar #'make-game-pane games))))
 
+;; %% LEAGUE (ALL THE TEAMS) PANE --------------------------------------------------------------------------------------
+
 ;; %% TOP LEVEL APPLICATION FRAMES -------------------------------------------------------------------------------------
 
 (define-application-frame team-info ()
   ( (team  :initarg :team :initform :phi) )
-  (:panes (team-icon    (with-slots (team) *application-frame* (make-team-icon-pane-large team)))
-          (team-banner  (with-slots (team) *application-frame* (make-team-banner-pane team t)))
+  (:panes (team-icon    (with-slots (team) *application-frame*
+                          (make-team-icon-pane team +icon-large+ nil
+                                               :west +game-list-item-west-border-size+
+                                               :east +game-list-item-inner-border-size+)))
+          (team-banner  (with-slots (team) *application-frame* (make-team-banner-pane team nil)))
           (team-sched   (with-slots (team) *application-frame*
                           (scrolling (:scroll-bar :vertical :height 400) (make-team-schedule-pane team)))))
   (:layouts (default (vertically () (horizontally () team-icon team-banner) team-sched)))
@@ -274,6 +287,13 @@
 (define-application-frame game-info ()
   ( (game :initarg :game) )
   (:pane (with-slots (game) *application-frame* (make-game-pane game)))
+  (:menu-bar nil))
+
+(define-application-frame league-info ()
+  ( )
+  (:panes (nfc (make-conference-pane :nfc))
+          (afc (make-conference-pane :afc)))
+  (:layouts (default (horizontally () afc nfc)))
   (:menu-bar nil))
 
 (defun run-team (team) (run-frame-top-level (make-application-frame 'team-info :team team)))
--- scratch.lisp
+++ scratch.lisp
@@ -9,47 +9,6 @@
 
 (in-package :app)
 
-;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-;; Main application frame, with an application window and an interactor window
-;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-(define-application-frame superapp ()
-  ()
-  (:pointer-documentation t)
-  (:panes
-    ;; Let's add an additional pane
-    (app :application
-          ;; :DISPLAY-TIME specifies when this pane should be displayed
-          ;; in the command loop. Note that the refresh is
-          ;; pane-specific, not application-wide.
-          :display-time nil
-          :height 400
-          :width 600)
-    (int  :interactor
-          :height 200
-          :width 600))
-  (:layouts
-    ;; This time we explicitly specify that the 2 defined panes
-    ;; should be stacked vertically.
-    (default
-      (vertically () app int))))
-
-;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-;; Let's also define commands that will act on the application.
-;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-;; How to leave the application.
-;; Note the '-superapp-' part of the command definition, coming from
-;; the name of the application frame.
-(define-superapp-command (com-quit :name t) ()
-  (frame-exit *application-frame*))
-
-;; This is an additional command that will be used in the next
-;; example, so its content is not important. However, it is useful to
-;; describe some aspects of the command loop. See below.
-(define-superapp-command (com-parity :name t) ((number 'integer))
-  (format t "~a is ~a~%" number (if (oddp number) "odd" "even")))
-
 (defun run-app () (run-frame-top-level (make-application-frame 'superapp)))
 
 (load "nfl-db.lisp")
@@ -60,3 +19,65 @@
 
 (load-data)
 (load "nfl-tool.lisp")
+
+
+;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+;; Try out presentation types
+;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+(define-presentation-method accept ((type nfl-db::team) stream view &key)
+  (let ( (team-id (accept '(member buf mia ne nyj bal cin cle pit hou ind jax ten den kc lv lac
+                                   dal nyg phi was chi det gb min atl car no tb ari la sf sea)
+                          :stream stream
+                          :prompt "?")) )
+    (make-team team-id)))
+
+; (in-package :clim-user)
+
+; (define-presentation-type ticket ())
+
+; (setf (get 'bush 'party) 'republican)
+; (setf (get 'quayle 'party) 'republican)
+; (setf (get 'clinton 'party) 'democrat)
+; (setf (get 'gore 'party) 'democrat)
+
+; ;;; separated by comma version
+; (define-presentation-method accept ((type ticket) stream view &key &allow-other-keys)
+;   (declare (ignore view))
+;   (let ( (president (accept '(member bush clinton)
+;                            :stream stream
+;                            :prompt nil
+;                            ;; add comma as a completing delimiter
+;                            :blip-characters '(#\,))) )
+;     ;; Make sure that the names were separated by a comma
+;     (unless (eql (read-gesture :stream stream) #\,) (simple-parse-error "Ticket members must be separated by commas"))
+
+;     (let ((veep (accept '(member quayle gore) :stream stream :prompt nil)))
+;       ;; Validate party affiliations
+;       (unless (eql (get president 'party) (get veep 'party))
+;         (simple-parse-error "Ticket members must be of the same party"))
+
+;       (list president veep))))
+
+; ;;; Separated by Return version
+; (define-presentation-method accept ((type ticket) stream view &key
+;                                    &allow-other-keys)
+;  (declare (ignore view))
+;  (let ((president (accept '(member bush clinton)
+;                           :stream stream
+;                           :prompt nil
+;                           ;; Remove Newline from activation characters
+;                           :activation-characters `()
+;                           ;; Add Newline as a delimiter, so that we get
+;                           ;; completion and move-to-next-field behavior
+;                           ;; when Return is typed.
+;                           :blip-characters `(#\Return #\Newline))))
+;    (unless (eql (read-gesture :stream stream) #\Newline)
+;      (simple-parse-error "Ticket members must be entered on separate lines"))
+
+;    (let ( (veep (accept '(member quayle gore) :stream stream :prompt nil)) )
+;      ;; Validate party affiliations
+;      (unless (eql (get president 'party) (get veep 'party))
+;        (simple-parse-error "Ticket members must be of the same party"))
+
+;      (list president veep))))
